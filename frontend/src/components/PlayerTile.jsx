83bda75675b482fde1a88304e589d63ce6b2914f
Oliver Starkey <132859794+91b91@users.noreply.github.com>
2025-01-24 09:34:58 +1100

    Player turn animations and tichu animations

--- frontend/src/components/PlayerTile.jsx
+++ frontend/src/components/PlayerTile.jsx
@@ -1,22 +1,15 @@
 export function PlayerTile({ user }) {
   return (
     <div className="player-tile-container">
+      {user.isTichu && <div className="tichu-animation-container"></div>}
       <div className="tile-profile-picture">
         {user.name.charAt(0).toUpperCase()}
       </div>
       <div className="name-and-tag-container">
-        <div className="tile-player-name">
-          {user.name}
-        </div>
-        {user.isTichu && (
-          <div className="tile-tag">
-            Tichu
-          </div>
-        )}
+        <div className="tile-player-name">{user.name}</div>
+        {user.isTichu && <div className="tile-tag">Tichu</div>}
       </div>
-      <div className="tile-cards-remaining">
-        {user.hand.length}
-      </div>
+      <div className="tile-cards-remaining">{user.hand.length}</div>
     </div>
-  )
+  );
 }
